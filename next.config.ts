4618a8976a40d76a955088c9de9455147fca8ffe
Nirav Chavda <nmchavda99@gmail.com>
2024-10-31 12:24:30 +0530

    ENH: Add CI pipeline setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/gen-forge",
+  assetPrefix: '/gen-forge/',
 };
 
 export default nextConfig;
